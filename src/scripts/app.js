13fcb0a2d06641360e8e45b068f94be7a6628891
Dominik Zając <zajac_dominik@onet.pl>
2019-02-18 19:12:24 +0100

    Add script to section feedback

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,13 @@
-console.log('hello');
+var feedbackSlider = tns({
+  container: '.feedback-container',
+  mouseDrag: true,
+  loop: true,
+  items: 1,
+  speed: 4000,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  controls: false,
+  controlsText: false,
+  navContainer: '.feedback-dots'
+});
