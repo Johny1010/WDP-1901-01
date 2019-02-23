093909bc777e0ebab7ad9fe11e3618815a306b77
Anna Strzała <strzala.ania@gmail.com>
2019-02-23 14:46:16 +0100

    Add tiny-slider functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+/* TINY SLIDER - NEW FURNITURE */
+
+const slider = tns({
+  container: '.my-slider',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  mouseDrag: true,
+  speed: 400,
+  responsive: {
+    768: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
